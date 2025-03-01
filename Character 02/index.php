7637e4893a443aa85a006ef3f660fbe403277a35
-v <zawlinn.profile@outlook.com>
2025-03-01 17:55:05 +0700

    PHP: Inter Course Update

---
+++ Character 02/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "./assets/view/header.php";
+
+?>
+
+<div class="container">
+    <h3 class="text-primary">How to Create a Class &mdash;</h3>
+
+    <?php
+
+ 
+
+    ?>
+
+</div>
+
+<?php
+    include_once "./assets/view/footer.php";
+?>
